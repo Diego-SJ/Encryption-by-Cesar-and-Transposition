9a3c4ad3bb757aa2a0a99ca0cc01baeac29f8b1c
Diego Salas <dsalas035@gmail.com>
2020-02-11 03:25:59 -0600

    final version

--- decrypt.py
+++ decrypt.py
@@ -1,52 +1,144 @@
 from io import open
 
-# Read file
-txtFile = open('message_encrypt.txt','r', encoding="utf8")
-fileContent = txtFile.readlines()
-txtFile.close()
+def get_number_location(key, kywrd_num_list):
+    num_loc = ""
+    for i in range(len(key)):
+        for j in range(len(key)):
+            if kywrd_num_list[j] == i:
+                num_loc += str(j)
+            # if
+        # for
+    # for
+    return num_loc
 
-# Replace returns by spaces
-encrypted_text = ""
-for fc in fileContent:
-    encrypted_text += fc.replace("\n", " ")
 
-# Print the plaint text
-print("ENCRYPTED TEXT: ",encrypted_text)
+def keyword_num_assign(key):
+    alpha = "ABCDEFG"
+    kywrd_num_list = list(range(len(key)))
+    # print(kywrdNumList)
+    init = 0
+    for i in range(len(alpha)):
+        for j in range(len(key)):
+            if alpha[i] == key[j]:
+                init += 1
+                kywrd_num_list[j] = init - 1
+            # if
+        # inner for
+    # for
+    return kywrd_num_list
 
-# Alphabet
-alphabet = "abcdefghijklmnñopqrstuvwxyz1234567890"
- 
-# Scroll key
-key = int (input("KEY: "))
 
-if key > 0 and key <= 37:
-    # Decrypted text variable
-    decrypted_text = ""
-    
-    # Cesar Decrypt method
-    for et in encrypted_text:
-        if et == et.upper():
-            if et.lower() in alphabet:
-                decrypted_text += alphabet[((alphabet.index(et.lower()) - key) % (len(alphabet)))].upper()
-            else:
-                if et == " ":
-                    decrypted_text += et
-                else:
-                    decrypted_text += ""
+def transposition_decryption(encrypted_text):
+    msg = encrypted_text
+    # print(msg)
+    key = input("\n---------- Transposition keyword: ").upper()
+
+    # assigning numbers to keywords
+    kywrd_num_list = keyword_num_assign(key)
+
+    num_of_rows = int(len(msg) / len(key))
+
+    # getting locations of numbers
+    num_loc = get_number_location(key, kywrd_num_list)
+
+    # Converting message into a grid
+    arr = [[0] * len(key) for i in range(num_of_rows)]
+
+    # decipher
+    plain_text = ""
+    k = 0
+    itr = 0
+
+    # print(arr[6][4])
+    # itr = len(msg)
+
+    for i in range(len(msg)):
+        d = 0
+        if k == len(key):
+            k = 0
         else:
+            d: int = int(num_loc[k])
+        for j in range(num_of_rows):
+            arr[j][d] = msg[itr]
+            # print("j: {} d: {} m: {} l: {} ". format(j, d, msg[l], l))
+            itr += 1
+        if itr == len(msg):
+            break
+        k += 1
+    print()
+
+    for i in range(num_of_rows):
+        for j in range(len(key)):
+            plain_text += str(arr[i][j])
+        # for
+    # for
+
+    print("\n\n========== Cesar encription ==========\n\n")
+    print(plain_text)
+
+    return plain_text
+
+def cesar_decryption(final_encryption):
+
+    encrypted_text = final_encryption.replace(",", " ")
+
+    # Alphabet
+    alphabet = "ABCDEFGHIJKLMNÑOPQRSTUVWXYZ1234567890"
+    
+    # Scroll key
+    key = int (input("\n---------- Cesar's key: "))
+
+    if key > 0 and key <= 37:
+        # Decrypted text variable
+        decrypted_text = ""
+        
+        # Cesar Decrypt method
+        for et in encrypted_text:
             if et in alphabet:
-                decrypted_text += alphabet[((alphabet.index(et) - key) % (len(alphabet)))]
+                    decrypted_text += alphabet[((alphabet.index(et) - key) % (len(alphabet)))]
             else:
                 if et == " ":
                     decrypted_text += et
                 else:
                     decrypted_text += ""
-    
-    # Print Decrypted text
-    print("DECRYPTED TEXT: ",decrypted_text)
 
+        print("\n\n========== Plain text decrypted ==========\n\n")
+        print(decrypted_text)
+        
+        return decrypted_text
+        
+    else:
+        print("Key out of range")
+
+def main():
+    # Read file
+    txtFile = open('message_encrypt.txt','r', encoding="utf8")
+    fileContent = txtFile.readlines()
+    txtFile.close()
+
+    # Replace returns by spaces
+    encrypted_text = ""
+    for fc in fileContent:
+        encrypted_text += fc
+
+    # Print the encrypted text
+    print("\n\n========== Encrypted text (transposition) ==========\n\n")
+    print(encrypted_text)
+
+    # Run transposition decryption
+    to_cesar = transposition_decryption(encrypted_text)
+    
+    # Run cesar decryption
+    final_result = cesar_decryption(to_cesar)
+    
+    # Create message_decrypt.txt file
     outputFile = open('message_decrypted.txt','w')
-    outputFile.write(decrypted_text)
+    outputFile.write(final_result)
     outputFile.close()
-else:
-    print("Key out of range")
+    print("\n\n========== Check message_decrypt.txt file ==========\n\n") 
+
+if __name__ == "__main__":
+    main()
+
+
+
--- encrypt.py
+++ encrypt.py
@@ -1,51 +1,160 @@
 from io import open
 
-# Read file
-txtFile = open('message.txt','r', encoding="utf8")
-fileContent = txtFile.readlines()
-txtFile.close()
+def encrypt_transposition(encrypted_text):
+    # msg = input("Enter Plain Text: ").replace(" ", "").upper()
+    msg = encrypted_text.replace(" ", ",")
+    # print(msg)
+    key = input("\n---------- Transposition keyword: ").upper()
 
-# Replace returns by spaces
-plain_text = ""
-for fc in fileContent:
-    plain_text += fc.replace("\n", " ")
+    # assigning numbers to keywords
+    kywrd_num_list = keyword_num_assign(key)
 
-# Print the plaint text
-print("PLAIN TEXT: ",plain_text)
+    print("\n\n========== Transposition grid ==========\n\n")
+    # printing key
+    for i in range(len(key)):
+        print(key[i], end=" ", flush=True)
+    # for
+    print()
+    for i in range(len(key)):
+        print(str(kywrd_num_list[i]), end=" ", flush=True)
+    # for
+    print("\n-------------------------")
 
-# Alphabet
-alphabet = "abcdefghijklmnñopqrstuvwxyz1234567890"
-# Scroll key
-key = int (input("KEY: "))
- 
-if key > 0 and key <= 37:
-    # Encrypted text variable
-    encrypted_text = ""
+    # in case characters don't fit the entire grid perfectly.
+    extra_letters = len(msg) % len(key)
+    # print(extraLetters)
+    dummy_characters = len(key) - extra_letters
+    # print(dummyCharacters)
+
+    if extra_letters != 0:
+        for i in range(dummy_characters):
+            msg += "."
+    # if
+
+    # print(msg)
+
+    num_of_rows = int(len(msg) / len(key))
+
+    # Converting message into a grid
+    arr = [[0] * len(key) for i in range(num_of_rows)]
+    z = 0
+    for i in range(num_of_rows):
+        for j in range(len(key)):
+            arr[i][j] = msg[z]
+            z += 1
+        # for
+    # for
+
+    for i in range(num_of_rows):
+        for j in range(len(key)):
+            print(arr[i][j], end=" ", flush=True)
+        print()
+    # for
+
+    # getting locations of numbers
+    num_loc = get_number_location(key, kywrd_num_list)
     
-    # Cesar encryption method
-    for pt in plain_text:
-        if pt == pt.upper():
-            if pt.lower() in alphabet:
-                encrypted_text += alphabet[((alphabet.index(pt.lower()) + key) % (len(alphabet)))].upper()
-            else:
-                if pt == " ":
-                    encrypted_text += pt
-                else:
-                    encrypted_text += ""
+    # cipher
+    cipher_text = ""
+    k = 0
+    for i in range(len(key)):
+        if k == len(key):
+            break
         else:
+            d = int(num_loc[k])
+        # if
+        for j in range(num_of_rows):
+            cipher_text += arr[j][d]
+        # for
+        k += 1
+    # for
+
+    print("\n\n========== Final Text Encrypted ==========\n\n")
+    print(cipher_text)
+    
+    return cipher_text
+
+def get_number_location(key, kywrd_num_list):
+    num_loc = ""
+    for i in range(len(key)):
+        for j in range(len(key)):
+            if kywrd_num_list[j] == i:
+                num_loc += str(j)
+            # if
+        # for
+    # for
+    return num_loc
+
+
+def keyword_num_assign(key):
+    alpha = "ABCDEFG"
+    kywrd_num_list = list(range(len(key)))
+    # print(kywrdNumList)
+    init = 0
+    for i in range(len(alpha)):
+        for j in range(len(key)):
+            if alpha[i] == key[j]:
+                init += 1
+                kywrd_num_list[j] = init - 1
+            # if
+        # inner for
+    # for
+    return kywrd_num_list
+
+def encrypt_cesar(plain_text):
+    # Alphabet
+    alphabet = "ABCDEFGHIJKLMNÑOPQRSTUVWXYZ1234567890"
+    # Scroll key
+    key = int (input("\n---------- Cesar's key: "))
+    
+    if key > 0 and key <= 37:
+        # Encrypted text variable
+        encrypted_text = ""
+        
+        # Cesar encryption method
+        for pt in plain_text:
             if pt in alphabet:
-                encrypted_text += alphabet[((alphabet.index(pt) + key) % (len(alphabet)))]
+                    encrypted_text += alphabet[((alphabet.index(pt) + key) % (len(alphabet)))]
             else:
                 if pt == " ":
                     encrypted_text += pt
                 else:
                     encrypted_text += ""
-    
-    # Print encrypted text
-    print("ENCRYPTED TEXT: ",encrypted_text)
+        
+        # Print encrypted text
+        print("\n\n========== Text encrypted by cesar ==========\n\n",encrypted_text)
 
+        return encrypted_text
+    
+    else:
+        print("Key out of range")
+
+def main():
+    
+    # Read file
+    txtFile = open('message.txt','r', encoding="utf8")
+    fileContent = txtFile.readlines()
+    txtFile.close()
+
+    # Replace returns by spaces
+    plain_text = ""
+    for fc in fileContent:
+        plain_text += fc.replace("\n", " ").upper()
+
+    # Print the plaint text
+    print("\n\n========== Plain Text from message.txt file ==========\n\n",plain_text)
+
+    # Run cesar encryption
+    encrypted_text = encrypt_cesar(plain_text)
+
+    # Run transposition encryption
+    final_encryption = encrypt_transposition(encrypted_text)
+
+    # Create output file
     outputFile = open('message_encrypt.txt','w')
-    outputFile.write(encrypted_text)
+    outputFile.write(final_encryption)
     outputFile.close() 
-else:
-    print("Key out of range")
+    print("\n\n========== Check message_encrypt.txt file ==========\n\n") 
+
+if __name__ == "__main__":
+    main()
