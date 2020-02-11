3df62ecbd5ce693b48a2844cf6223f878199f006
Diego Salas <dsalas035@gmail.com>
2020-02-10 23:46:09 -0600

    Cesar decrypt fixed

--- decrypt.py
+++ decrypt.py
@@ -14,27 +14,39 @@ for fc in fileContent:
 print("ENCRYPTED TEXT: ",encrypted_text)
 
 # Alphabet
-alphabet = "abcdefghijklmnñopqrstuvwxyzABCDEFGHIJKLMNÑOPQRSTUVWXYZ1234567890"
+alphabet = "abcdefghijklmnñopqrstuvwxyz1234567890"
  
 # Scroll key
 key = int (input("KEY: "))
- 
-# Decrypted text variable
-decrypted_text = ""
- 
-# Cesar Decrypt method
-for pt in encrypted_text:
-    if pt in alphabet:
-        decrypted_text += alphabet[(alphabet.index(pt) - key % (len(alphabet)))]
-    else:
-        if pt == " ":
-            decrypted_text += pt
-        else:
-            decrypted_text += ""
- 
-# Print Decrypted text
-print("DECRYPTED TEXT: ",decrypted_text)
 
-outputFile = open('message_decrypted.txt','w')
-outputFile.write(decrypted_text)
-outputFile.close()
+if key > 0 and key <= 37:
+    # Decrypted text variable
+    decrypted_text = ""
+    
+    # Cesar Decrypt method
+    for et in encrypted_text:
+        if et == et.upper():
+            if et.lower() in alphabet:
+                decrypted_text += alphabet[((alphabet.index(et.lower()) - key) % (len(alphabet)))].upper()
+            else:
+                if et == " ":
+                    decrypted_text += et
+                else:
+                    decrypted_text += ""
+        else:
+            if et in alphabet:
+                decrypted_text += alphabet[((alphabet.index(et) - key) % (len(alphabet)))]
+            else:
+                if et == " ":
+                    decrypted_text += et
+                else:
+                    decrypted_text += ""
+    
+    # Print Decrypted text
+    print("DECRYPTED TEXT: ",decrypted_text)
+
+    outputFile = open('message_decrypted.txt','w')
+    outputFile.write(decrypted_text)
+    outputFile.close()
+else:
+    print("Key out of range")
